cb698696828ed5f0d823325a7355866e3fb6afd6
jeruxniviz <jeruxniviz@gmail.com>
2022-09-18 15:28:43 +0700

    tugas 1

---
+++ bank.py
@@ -0,0 +1,8 @@
+greeting = input("Greeting : ").strip().lower()
+
+if greeting=="hello" :
+    print("output = $0")
+elif greeting[0]=="h" : 
+    print("output = $20")
+else : 
+    print("output = $100")
