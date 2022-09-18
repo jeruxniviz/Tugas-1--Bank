b2be9addf0ac6e46f13adb2853c0b2663a5808f8
jeruxniviz <jeruxniviz@gmail.com>
2022-09-18 18:58:10 +0700

    bank

--- bank.py
+++ bank.py
@@ -4,8 +4,6 @@ greeting = input("Greeting : ").strip().lower()
 
 if greeting=="hello" or greeting[0:5]=="hello":
     print("output = $0")
-elif "hello" in greeting : #cek apakah ada hello dalam kalimat
-    print("output = $0")
 elif greeting[0]=="h" : 
     print("output = $20")
 else : 
